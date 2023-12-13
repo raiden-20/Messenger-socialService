f4515e7441f4a74e124a1c8854d3b09a32fd44f0
undine_20 <lena_sheina@bk.ru>
2023-12-13 12:03:36 +0300

    initial commit

---
+++ src/main/java/ru/vsu/cs/socialservice/SocialServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.socialservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialServiceApplication.class, args);
+    }
+
+}
