9953326d8cd7872234624e7603c2d15b461b4b97
undine_20 <lena_sheina@bk.ru>
2023-12-13 13:48:25 +0300

    add rabbit support

---
+++ src/main/java/ru/vsu/cs/socialservice/configuration/RabbitConfig.java
@@ -0,0 +1,20 @@
+package ru.vsu.cs.socialservice.configuration;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class RabbitConfig {
+    @Bean
+    public Jackson2JsonMessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public Queue mailSenderQueue() {
+        return new Queue(RabbitQueues.fromAuthQueue);
+    }
+}
---
+++ src/main/java/ru/vsu/cs/socialservice/configuration/RabbitQueues.java
@@ -0,0 +1,5 @@
+package ru.vsu.cs.socialservice.configuration;
+
+public class RabbitQueues {
+    public static final String fromAuthQueue = "FROM_AUTH_QUEUE";
+}
