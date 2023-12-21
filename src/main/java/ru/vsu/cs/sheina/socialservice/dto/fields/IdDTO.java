b580f921454405be1a6142d2da2c227dca560c47
undine_20 <lena_sheina@bk.ru>
2023-12-21 18:28:43 +0300

    fix some annotation

--- src/main/java/ru/vsu/cs/sheina/socialservice/dto/fields/IdDTO.java
+++ src/main/java/ru/vsu/cs/sheina/socialservice/dto/fields/IdDTO.java
@@ -1,10 +1,12 @@
 package ru.vsu.cs.sheina.socialservice.dto.fields;
 
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import java.util.UUID;
 
 @Data
+@RequiredArgsConstructor
 public class IdDTO {
     UUID id;
 }
--- src/main/java/ru/vsu/cs/sheina/socialservice/dto/rabbitmq/UrlDTO.java
+++ src/main/java/ru/vsu/cs/sheina/socialservice/dto/rabbitmq/UrlDTO.java
@@ -2,9 +2,11 @@ package ru.vsu.cs.sheina.socialservice.dto.rabbitmq;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class UrlDTO {
 
     String sourceId;
